2f853e4aeb7fec8240e7d4192eb66a1d0b4ee054
INnoVatioN-97 <koyoungil97@gmail.com>
2024-04-28 20:28:37 +0900

    chore: 상대경로 alias 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import react from '@vitejs/plugin-react';
+import path, { resolve } from 'path';
 import { defineConfig } from 'vite';
+const __dirname = path.resolve();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      utils: resolve(__dirname, './src/utils'),
+    },
+  },
 });
